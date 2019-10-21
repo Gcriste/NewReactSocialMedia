604a3ac43918ab1bd3c515307532dd5a896b00e6
Griffin <griffincriste@gmail.com>
2019-10-21 17:57:44 -0500

    add posts

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,9 +1,11 @@
 import {
     GET_POSTS,
     POST_ERROR,
-    UPDATE_LIKES
+    UPDATE_LIKES,
+    DELETE_POST,
+    ADD_POST,
+    GET_POST
 } from '../actions/types';
-import { startSession } from 'mongoose';
 
 
 const initialState = {
@@ -20,20 +22,38 @@ export default function(state = initialState, action ){
         case GET_POSTS:
             return {
                 ...state,
-                posts:payload,
-                loading:false
+                posts: payload,
+                loading: false
             };
+        case GET_POST:
+            return {
+                ...state,
+                post: payload,
+                loading: false
+            }
+        case ADD_POST:
+            return {
+                ...state,
+                posts: [payload,...state.posts],
+                loading: false
+            }
+        case DELETE_POST:
+            return {
+                ...state,
+                posts: state.posts.filter(post => post._id !== payload),
+                loading: false
+            }
         case POST_ERROR:
             return {
                 ...state,
-                error:payload,
-                loading:false
+                error: payload,
+                loading: false
             };
         case UPDATE_LIKES:
             return {
                 ...state,
-                posts:state.posts.map(post => post._id === payload.id ?{ ...post, likes: payload.likes} : post),
-                loading:false
+                posts: state.posts.map(post => post._id === payload.id ?{ ...post, likes: payload.likes} : post),
+                loading: false
             }
         default:
                 return state;
