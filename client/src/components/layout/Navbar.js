27b20a661a72f6c6c88e3f0734e76c9fd83aa759
Griffin <griffincriste@gmail.com>
2019-10-21 09:48:58 -0500

    got user profile based on whose logged in, working on landing page and routing for dashboard

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,17 +7,10 @@ import { logout } from '../../actions/auth';
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
-      <li>
-        <Link to='/profiles'>Developers</Link>
-      </li>
-      <li>
-        <Link to='/posts'>Posts</Link>
-      </li>
-      <li>
+    <li>
         <Link to='/dashboard'>
-          <i className='fas fa-user' />{' '}
-          <span className='hide-sm'>Dashboard</span>
-        </Link>
+        <i className='fas fa-user' />{' '}
+        <span className='hide-sm'>Dashboard</span></Link>
       </li>
       <li>
         <a onClick={logout} href='#!'>
