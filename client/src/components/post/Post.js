54530a7a197ee774ffcce01f4fc01dd40b80be27
Griffin <griffincriste@gmail.com>
2019-10-21 18:29:11 -0500

    fully functional app

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,7 +4,10 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentForm from '../post/CommentForm';
+import CommentItem from '../post/CommentItem';
 import { getPost } from '../../actions/post';
+
 
 
 const Post = ({ getPost, post: { post, loading }, match })=> {
@@ -12,13 +15,25 @@ const Post = ({ getPost, post: { post, loading }, match })=> {
         getPost(match.params.id)
     }, [getPost]);
 
-    return loading || post === null ? <Spinner /> : <Fragment>
+    return loading || post === null ? ( <Spinner /> ): 
+    (<Fragment>
         <Link to ='/posts' className = 'btn'>
             Back to Posts
         </Link>
         <PostItem post = {post} 
         showActions = {false}/>
+        <CommentForm postId = {post._id} />
+        <div className="comments">
+            {post.comments.map(comment => (
+                <CommentItem 
+                key ={comment._id} 
+                comment = {comment}
+                postId = {post._id}
+                />
+            ))}
+        </div>
     </Fragment>
+    );
 };
 
 Post.propTypes = {
